d793add0098829f23b60bc75bef7cb8d1c44fbb7
Prathamesh Bhatagalikar <prathamesh.bhatagalikar2@cognizant.com>
2026-04-03 18:59:46 +0530

    feature: signup page creation

--- apps/storefront/src/router/index.ts
+++ apps/storefront/src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import LoginPage from '../pages/LoginPage.vue'
+import SignUpPage from '../pages/SignUpPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomePage },
     { path: '/login', component: LoginPage },
+    { path: '/signup', component: SignUpPage}
   ],
 })
 
